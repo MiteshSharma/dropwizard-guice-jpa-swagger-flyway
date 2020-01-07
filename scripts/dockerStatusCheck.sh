12f31094738570df06eb6e99a3a7dadbe34d0b06
Mitesh Sharma <Mitesh@Admins-MacBook-Pro-4.local>
2020-01-08 00:36:04 +0530

    Updating script with correct messages

--- scripts/dockerStatusCheck.sh
+++ scripts/dockerStatusCheck.sh
@@ -4,10 +4,10 @@ checkStatus() {
   while true; do
     STATUS=$(docker inspect --format "{{json .State.Health.Status }}" $1)
     if [[ $STATUS == "\"healthy\"" ]]; then
-      echo "Docker $name is up and running"
+      echo "Docker $1 is up and running"
       break
     fi
-    >&2 echo "Docker $name is unavailable, waiting to start...";
+    echo "Docker $1 status is $STATUS, waiting to be healthy...";
     sleep 3;
   done
 }
