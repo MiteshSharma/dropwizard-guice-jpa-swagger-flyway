8fff03f8721c38fd53fb99016261fb1ead486f0c
Mitesh Sharma <Mitesh@Admins-MacBook-Pro-4.local>
2020-01-13 19:25:32 +0530

    Adding index resource to handle check whether app  is running

---
+++ src/main/java/com/myth/resources/IndexResource.java
@@ -0,0 +1,36 @@
+package com.myth.resources;
+
+import com.codahale.metrics.annotation.Timed;
+import com.google.inject.Inject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.eclipse.jetty.http.HttpStatus;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+@Api(value="/")
+@Produces(MediaType.APPLICATION_JSON)
+public class IndexResource {
+    @Inject
+    public IndexResource() {
+    }
+
+    @GET
+    @Path("/")
+    @Timed
+    @ApiOperation(
+            value = "Get status of server",
+            notes = "Returns HTTP OK when this API is called.")
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpStatus.OK_200, message = "")})
+    public Response get() {
+        return Response.status(Response.Status.OK).build();
+    }
+}
